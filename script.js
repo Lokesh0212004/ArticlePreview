487326fab3a9e1db4e309936566f61225ca538f4
Lokesh S <153573033+Lokesh0212004@users.noreply.github.com>
2024-04-29 16:53:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var btn=document.getElementById('share');
+var shareopt=document.getElementById('pops')
+
+
+btn.addEventListener("click",()=>{
+    shareopt.classList.toggle('active')
+
+})
+// btn.addEventListener("keyup")
+
+
+
+
